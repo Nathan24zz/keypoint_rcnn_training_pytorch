807abd9e3523fc539376fd442fa54c6701604cd1
nathanael.hutama24@gmail.com <nathanterbaik@gmail.com>
2023-05-25 01:54:56 +0700

    fix: change kpt_oks_sigmas to 0.4 (same with nimbro), add line for visualization

--- engine.py
+++ engine.py
@@ -9,7 +9,7 @@ from coco_eval import CocoEvaluator
 from coco_utils import get_coco_api_from_dataset
 
 
-def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
+def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq, writer):
     model.train()
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value:.6f}"))
@@ -29,10 +29,8 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         loss_dict = model(images, targets)
-
         losses = sum(loss for loss in loss_dict.values())
-
-        # reduce losses over all GPUs for logging purposes
+        writer.add_scalar('loss/train', losses.item(), epoch + 1)
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
 
